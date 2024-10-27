c345def5768652a3dcec4ec88f7c78b29657c8cd
Cesar <cesarwamartinez@gmail.com>
2024-10-26 20:12:40 -0500

    No me acuerdo que es

--- vite-project/src/pages/DataList.jsx
+++ vite-project/src/pages/DataList.jsx
@@ -5,16 +5,17 @@ const DataTablePage = () => {
 
   return (
     <section className="hero">
-      <div class="field container">
+        <form action="datos/post" method="POST" className="container mb-3">
+        <div class="field container">
         <label class="label">Nombre table Query</label>
         <div class="control">
-          <input
-            class="input"
-            type="text"
-            placeholder="Ej: Activo"
-          />
+          <input class="input" type="text" placeholder="Ej: Activo"/>
+        </div>
+        <div className="buttons mt-3">
+            <button type="submit" className="is-link is-light button">Enviar</button>
         </div>
       </div>
+        </form>
       <DataTableQuery></DataTableQuery>
     </section>
   );
